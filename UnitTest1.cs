5f75060a4c7b4ea33a032b5a161e462892009453
MoonJing <jing1999fan@outlook.com>
2022-04-20 04:31:12 -0500

    1st upload
    the way to get the minimum edit distance and  its test code

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MED;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MED_Test()
+        {
+            string before = "banana";
+            string after = "baanan";
+            Assert.AreEqual(2, getMED.MED(before, after)) ;
+        }
+    }
+}
---
+++ med.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MED
+{
+    public class getMED
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("source string: ");
+            string source = Console.ReadLine();
+            //Console.WriteLine();
+
+            Console.Write("target string: ");
+            string target = Console.ReadLine();
+            //Console.WriteLine();
+
+
+            Console.WriteLine("Minimum Edit Distance: " + MED(source, target));
+            //Console.Write();
+            Console.ReadLine();
+        }
+        public static int MED(string source, string target)
+        {
+            int sSize = source.Length;
+            int tSize = target.Length;
+
+            if (sSize == 0)
+            {
+                return tSize;
+            }
+            if (tSize == 0)
+            {
+                return sSize;
+            }
+            int[,] matrix = new int[tSize + 1, sSize + 1];
+            for (int i = 0; i <= sSize; i++)
+            {
+                matrix[0, i] = i;
+            }
+            for (int j = 1; j <= tSize; j++)
+            {
+                matrix[j, 0] = j;
+            }
+            for (int i = 0; i < tSize; i++)
+            {
+                for (int j = 0; j < sSize; j++)
+                {
+                    int sign;
+                    if (source[j].Equals(target[i]))
+                        sign = 0;
+                    else
+                        sign = 1;
+                    matrix[i + 1, j + 1] = Math.Min(Math.Min(matrix[i, j] + sign, matrix[i + 1, j] + 1), matrix[i, j + 1] + 1);
+                }
+            }
+            return matrix[tSize, sSize];
+        }
+
+    }
+}
